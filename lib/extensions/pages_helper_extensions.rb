7219000259d1ecc1d8bc6d64c08eea11626ce200
José Fernández Alameda <jose.fernandez.alameda@gmail.com>
2013-07-28 04:34:45 +0200

    Final adjustments

--- lib/extensions/pages_helper_extensions.rb
+++ lib/extensions/pages_helper_extensions.rb
@@ -7,11 +7,24 @@ module Extensions
 	      	# Allows to retrieve a dynamic form value
 	      	# for a given page id and field id.
 	      	def get_field(field_id)
+
 	      		value = Refinery::Dynamicfields::DynamicformValue.find_by_page_and_field_id(@page.id, field_id)
 	      		if value
-	      			value[value.dynamicform_field.field_type+"_value"]
+	      			field_type = value.dynamicform_field.field_type
+	      			if(field_type == "string" || field_type == "text")
+	      				return value[field_type+"_value"]
+	      			end
+	      			if(field_type == "image")
+	      				return value.image
+	      			end
+	      			if(field_type == "file")
+	      				return value.resource
+	      			end
+	      			if(field_type == "wyswyg")
+	      				return value.text_value
+	      			end
 	      		else
-	      			""
+	      			return ""
 	      		end
 	      	end
 
